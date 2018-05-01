a4f448aad8fe11bea3ef6f7a5f1176cded2abc01
Evan Kuhn <kuhnep@miamioh.edu>
2018-05-01 19:44:23 -0400

    fixed syntax errors

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ import {
 export default class SwiftBleManager {
   getRssiValues() {
     console.log("Hello");
-    console.log(
-      "Constants available immediately: ",
-      NativeModules.stest.a,
-      NativeModules.stest.b
-    );
-    const e = new NativeEventEmitter(NativeModules.stest);
-    const s = e.addListener("Hello", (arr) => {
-      console.log("Received a hello event", arr.message);
-    })
-    console.log(
-      return NativeModules.BeaconTracker.getRssis()
-    );
+    return NativeModules.BeaconTracker.getRssis()
   }
 }
